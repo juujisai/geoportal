2ecb512f3e2f8c0c048a7ab85744e981db6383e1
Bartosz <bartosz.cylwik94@gmail.com>
2020-04-15 10:40:16 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const geoportalInit = function () {
+  const buttonContinue = document.querySelector('.continue');
+  const mobileHeader = document.querySelector('.mobileHeader');
+  buttonContinue.addEventListener('click', function () {
+    mobileHeader.classList.toggle('active')
+  })
+
+
+}
+
+geoportalInit()
+
+
+
+const toolsHandle = function () {
+  const layersMenu = document.querySelector('.tool.layer');
+  const layersList = document.querySelector('.layerList');
+  layersMenu.addEventListener('click', function () {
+    layersList.classList.toggle('active')
+  })
+
+}
+
+
+toolsHandle()
